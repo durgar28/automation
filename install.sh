1df4215df565d9eb2457f8daaf004fc70f166d8d
durgar28 <118597433+durgar28@users.noreply.github.com>
2022-11-24 02:14:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo "welcome"
 sudo -s
-apt upgrade
-apt update
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt update -y
 mkdir /home/durga
 cd /home/durga
-apt install vagrant
-apt install virtualbox 
+apt install virtualbox -y
+sleep 60
+apt install vagrant -y
+sleep 60
 vagrant box add ubuntu/trusty64  --force
-vagrant init  ubuntu/trusty64 
-vagrant up
-vagrant ssh 
+sleep 60
+vagrant init  ubuntu/trusty64
+sleep 60
+vagrant up 
+vagrant --provision
