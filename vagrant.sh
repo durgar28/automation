a87cd6e16d7006f3ace15e73495f1993adb9696c
durgar28 <118597433+durgar28@users.noreply.github.com>
2022-11-24 15:45:17 +0530

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -3,4 +3,12 @@ sudo mkdir test
 sudo apt install apache2 -y
 sleep 40
 sudo apt install docker.io -y
+sleep 40
+sudo apt update -y 
+sleep 40
+sudo apt install docker-ce -y
+sleep 60
+sudo usermod -aG docker ${USER}
+sudo newgrp docker
+sudo systemctl enable docker
 sudo docker swarm init
